18258b84fb7895e434aa6e615bb69ab140cef371
uaysev <utkua557@gmail.com>
2024-07-16 00:03:33 -0400

    drum machine is done

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import RandomQuoteMachine from './component/RandomQuote.jsx';
 import MarkdownPreviewer from "./component/MarkdownPreviewer";
+import DrumMachine from "./component/DrumMachine";
 import React from "react";
 
 function App() {
     return (
         <div className="App">
             {/*<RandomQuoteMachine />*/}
-            <MarkdownPreviewer></MarkdownPreviewer>
+            {/*<MarkdownPreviewer></MarkdownPreviewer>*/}
+            <DrumMachine></DrumMachine>
             <script src="https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js"></script>
 
         </div>
